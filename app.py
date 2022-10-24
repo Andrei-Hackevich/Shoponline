4b8bc377c749b15880400dad15b205f1d50c8201
Andrei Khatskevich <andrei_hackevich@mail.ru>
2022-10-24 19:50:44 +0300

    Создал поля для базы данных

--- app.py
+++ app.py
@@ -1,22 +1,38 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLACLHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+
 
 @app.route('/')
 @app.route('/home')
 def index():
     return render_template('index.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+
 @app.route('/user/<string:name>/<int:id>')
 def user(name, id):
     return 'User: ' + name + '-' + str(id)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
