901dab9f1fe167479deb7b56b0dcb4bacadbb04c
Andrei Khatskevich <andrei_hackevich@mail.ru>
2022-10-27 19:17:51 +0300

    Добавил окна для создания post запроса в create_article + сделал кнопку для перехода на страницу с добавлением записи + исправил ошибку бд в основном файле

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
