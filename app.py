68f08279b429f97aaf790cf74841d34f7f66cf6c
Andrei Khatskevich <andrei_hackevich@mail.ru>
2022-12-14 19:29:19 +0300

    Создал шаблон базовый и главной страницы, подключил бд

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///basa_visechek.db'
 
 
 @app.route('/')
