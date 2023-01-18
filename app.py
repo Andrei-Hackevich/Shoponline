738ab4878443f1dd2db187749dbe395953d7127f
Andrei Khatskevich <andrei_hackevich@mail.ru>
2023-01-18 19:24:06 +0300

    Создал шаблон для добавления высечки

--- app.py
+++ app.py
@@ -3,20 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///basa_visechek.db'
-db = SQLAlchemy
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 class Visechka(db.Model):
     id = db.Column(db.Integer, primary_key=True) #Порядковый номер высечки в базе данных
-    visechka_no = db.Column(db.Text, nullable=False) #Номер высечки присвоенный технологом
-    size = db.Column(db.Text, nullable=False) #Размер высечки
-    matherial = db.Column(db.Text, nullable=False)  #Для какого материала высечка
+    visechka_no = db.Column(db.String(20), nullable=False) #Номер высечки присвоенный технологом
+    size = db.Column(db.String(20), nullable=False) #Размер высечки
+    matherial = db.Column(db.String(20), nullable=False)  #Для какого материала высечка
     wide = db.Column(db.Integer, nullable=False) #ширина высечки в мм
     length = db.Column(db.Integer, nullable=False)  #длинна высечки в мм
     streams = db.Column(db.Integer, nullable=False) #Количество ручьев в высечке
     el_in_streams = db.Column(db.Integer, nullable=False)  #Количество элементов в одном ручье
     shaft = db.Column(db.Integer, nullable=False)  #Вал на котором располагается высечка
-    form = db.Column(db.Text, nullable=False) #форма высечки
+    form = db.Column(db.String(20), nullable=False) #форма высечки
     iznos = db.Column(db.Integer, nullable=False) #пробег высечки в м.п.
 
 
@@ -28,6 +29,11 @@ def main():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
 
 
 if __name__ == '__main__':
