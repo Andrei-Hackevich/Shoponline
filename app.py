0f5a5974ed0488ad688fa7e8be5101f01edd2ea8
Andrei Khatskevich <andrei_hackevich@mail.ru>
2023-02-16 15:30:47 +0300

    Создал возможность редактирования высечек и оптимизировал шаблоны

--- app.py
+++ app.py
@@ -39,43 +39,50 @@ def plenka():
     visechki = Visechka.query.filter(Visechka.matherial == 'Пленка').all()
     return render_template('plenka.html', visechki=visechki)
 
-@app.route('/plenka/<int:id>')
-def plenka_detail(id):
+@app.route('/<int:id>')
+def detail(id):
     visechka = Visechka.query.get(id)
-    return render_template('plenka_detail.html', visechka=visechka)
+    return render_template('detail.html', visechka=visechka)
 
-@app.route('/plenka/<int:id>/delete')
-def plenka_delete(id):
+@app.route('/<int:id>/delete')
+def delete(id):
     visechka = Visechka.query.get_or_404(id)
 
     try:
         db.session.delete(visechka)
         db.session.commit()
-        return redirect('/plenka')
+        return redirect('/')
     except:
         return "При удалении возникла ошибка"
+
+@app.route('/<int:id>/update', methods=['POST', 'GET'])
+def update(id):
+    visechka = Visechka.query.get(id)
+    if request.method == 'POST':
+        visechka.visechka_no = request.form['visechka_no']
+        visechka.matherial = request.form['matherial']
+        visechka.wide = request.form['wide']
+        visechka.length = request.form['length']
+        visechka.size = str(visechka.wide) + 'x' + str(visechka.length)
+        visechka.streams = request.form['streams']
+        visechka.el_in_streams = request.form['el_in_streams']
+        visechka.shaft = request.form['shaft']
+        visechka.form = request.form['form']
+        visechka.iznos = request.form['iznos']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'При добавлении записи возникла ощибка'
+    else:
+        visechka = Visechka.query.get(id)
+        return render_template('update.html', visechka=visechka)
 
 @app.route('/bumaga')
 def bumaga():
     visechki = Visechka.query.filter(Visechka.matherial == 'Бумага').all()
     return render_template('bumaga.html', visechki=visechki)
-
-@app.route('/bumaga/<int:id>')
-def bumaga_detail(id):
-    visechka = Visechka.query.get(id)
-    return render_template('bumaga_detail.html', visechka=visechka)
-
-@app.route('/bumaga/<int:id>/delete')
-def bumaga_delete(id):
-    visechka = Visechka.query.get_or_404(id)
-
-    try:
-        db.session.delete(visechka)
-        db.session.commit()
-        return redirect('/bumaga')
-    except:
-        return "При удалении возникла ошибка"
-
 
 @app.route('/create', methods=['POST', 'GET'])
 def create():
